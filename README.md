929ad618f20d09da841180bf787c8f574942946e
Matthieu Mota <matthieumota@gmail.com>
2024-10-31 12:04:52 +0100

    Poc node parsing

--- README.md
+++ README.md
@@ -11,3 +11,17 @@ npm run dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+## Explanations
+
+We need to implement functionalities on that test :
+
+- Parse data from CSV
+- Display that data on a tree
+- Interactions with nodes on that tree
+- CRUD on that tree (add / edit / delete)
+- Show metada from each node
+
+## Parse data
+
+Our first task is to parse data from csv source. We're going to parse data from server. We use stream to potentially read file with chunks for optimization (`createReadStream`). We use [csv-parser](https://www.npmjs.com/package/csv-parser) for that.
---
+++ src/app/api/nodes/route.ts
@@ -0,0 +1,59 @@
+import csvParser from 'csv-parser'
+import fs from 'fs'
+import path from 'path'
+
+export interface Node {
+  node_id: number
+  name: string
+  type: string
+  status: string
+  description: string
+  start_date: string
+  end_date: string
+  childrens: Array<Node>
+}
+
+export interface Relationship {
+  parent_id: number
+  child_id: number
+}
+
+async function readCSV<T>(filename: string): Promise<T[]> {
+  const file: string = path.join(process.cwd(), 'fixtures', filename)
+  const results: Array<T> = []
+
+  return await new Promise<Array<T>>((resolve, reject) => {
+    fs.createReadStream(file)
+      .on('error', (err) => reject(err))
+      .pipe(csvParser())
+      .on('data', (data: T) => results.push({ ...data, childrens: [] }))
+      .on('end', () => resolve(results))
+  })
+}
+
+export async function GET(request: Request): Promise<Response> {
+  try {
+    const nodes = await readCSV<Node>('nodes.csv')
+    const relations = await readCSV<Relationship>('relationships.csv')
+
+    const nodeMap = new Map<number, Node>()
+    nodes.forEach(node => nodeMap.set(node.node_id, node))
+
+    relations.forEach(({ parent_id, child_id }) => {
+      const parent = nodeMap.get(parent_id)
+      const child = nodeMap.get(child_id)
+
+      if (parent && child) {
+        parent.childrens.push(child)
+      }
+    })
+  
+    return Response.json({ nodes })
+  } catch (error) {
+    console.log(error)
+    return Response.json({
+      status: 500,
+      details: 'API not available',
+    })
+  }
+}
