0d41d8ce3c8a7bce4c35d483e4bcc1671d56f923
Matthieu Mota <matthieumota@gmail.com>
2024-11-01 23:39:03 +0100

    Validation

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,15 +1,15 @@
 import cn from 'classnames'
 import React, { MouseEvent } from 'react'
 
-interface Props {
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   onClick?: (event: MouseEvent) => void,
   children: React.ReactNode,
   className?: string
 }
 
-const Button: React.FC<Props> = ({ onClick, children, className }: Props): JSX.Element => {
+const Button: React.FC<Props> = ({ onClick, children, className, ...props }: Props): JSX.Element => {
   return (
-    <button className={cn(`px-6 py-2 rounded-lg shadow bg-gray-100 hover:bg-gray-50 duration-300`, className)} onClick={onClick}>
+    <button className={cn(`px-6 py-2 rounded-lg shadow bg-gray-100 hover:bg-gray-50 duration-300 disabled:opacity-50`, className)} onClick={onClick} {...props}>
       {children}
     </button>
   )
--- src/app/components/NodeModal.tsx
+++ src/app/components/NodeModal.tsx
@@ -1,5 +1,5 @@
 import { Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
-import React, { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent, useEffect, useMemo, useState } from 'react'
 import { Node } from './Tree'
 import Button from './Button'
 
@@ -11,17 +11,53 @@ interface Props {
 
 const NodeModal: React.FC<Props> = ({ node, onClose, onConfirm }: Props): JSX.Element | null => {
   const [newNode, setNewNode] = useState<Node | null>(node)
+  const [errors, setErrors] = useState<Record<string, string> | null>(null)
 
   if (!newNode) {
     return null
   }
 
-  const handleChange = (e: ChangeEvent, name: string) => {
-    setNewNode({ ...newNode, [name]: (e.target as HTMLInputElement).value })
+  useEffect(() => {
+    setErrors(validate(newNode))
+  }, [newNode])
+
+  const validate = (n: Node): Record<string, string> | null => {
+    const errors = {} as Record<string, string>
+
+    if (n.name.trim() === '') {
+      errors.name = 'Le nom est requis'
+    }
+
+    if (n.description.trim() === '') {
+      errors.description = 'La description est requise'
+    }
+
+    if (n.start_date === '') {
+      errors.start = 'La date de début est requise'
+    }
+
+    if (n.end_date === '') {
+      errors.end = 'La date de fin est requise'
+    }
+
+    const start = new Date(n.start_date)
+    const end = new Date(n.end_date)
+
+    if (start > end) {
+      errors.start = 'La date de début doit être antérieure à la date de fin'
+    }
+
+    if (Object.keys(errors).length === 0) {
+      return null
+    }
+
+    return errors
   }
 
-  const handleForm = () => {
-    onConfirm(newNode)
+  const valid = useMemo(() => errors === null || Object.keys(errors).length === 0, [errors])
+
+  const handleChange = (e: ChangeEvent, name: string) => {
+    setNewNode({ ...newNode, [name]: (e.target as HTMLInputElement).value })
   }
 
   return (
@@ -35,10 +71,11 @@ const NodeModal: React.FC<Props> = ({ node, onClose, onConfirm }: Props): JSX.El
         <div className="fixed inset-0 flex items-center justify-center p-4">
           <DialogPanel className="max-w-screen-md w-screen space-y-4 border bg-white p-6 rounded-xl shadow-xl">
             <DialogTitle className="font-bold">Noeud {node?.node_id}</DialogTitle>
-            <form onSubmit={handleForm}>
+            <form onSubmit={() => onConfirm(newNode)}>
               <div className="mb-3">
                 <label htmlFor="name" className="block mb-2">Nom</label>
                 <input className="w-full" type="text" id="name" value={newNode.name} onChange={(e) => handleChange(e, 'name')} />
+                {errors?.name && <p className="text-red-500 mt-2">{errors?.name}</p>}
               </div>
 
               <div className="mb-3">
@@ -62,20 +99,23 @@ const NodeModal: React.FC<Props> = ({ node, onClose, onConfirm }: Props): JSX.El
               <div className="mb-3">
                 <label htmlFor="description" className="block mb-2">Description</label>
                 <textarea className="w-full" id="description" value={newNode.description} onChange={(e) => handleChange(e, 'description')}></textarea>
+                {errors?.description && <p className="text-red-500 mt-2">{errors?.description}</p>}
               </div>
 
               <div className="mb-3">
                 <label htmlFor="start_date" className="block mb-2">Date de début</label>
                 <input className="w-full" type="date" id="start_date" value={newNode.start_date} onChange={(e) => handleChange(e, 'start_date')} />
+                {errors?.start && <p className="text-red-500 mt-2">{errors?.start}</p>}
               </div>
 
               <div className="mb-8">
                 <label htmlFor="end_date" className="block mb-2">Date de fin</label>
                 <input className="w-full" type="date" id="end_date" value={newNode.end_date} onChange={(e) => handleChange(e, 'end_date')} />
+                {errors?.end && <p className="text-red-500 mt-2">{errors?.end}</p>}
               </div>
 
               <div className="flex gap-4 flex-row-reverse">
-                <Button className="!bg-slate-600 hover:!bg-slate-500 text-white">Confirmer</Button>
+                <Button className="!bg-slate-600 hover:!bg-slate-500 text-white" disabled={!valid}>Confirmer</Button>
                 <Button onClick={onClose}>Annuler</Button>
               </div>
             </form>
